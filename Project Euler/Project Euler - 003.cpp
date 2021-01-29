fc237df72f8037dc36fc8e2636bf015194ab086b
Denis Zangarov <denisimo_98@yahoo.com>
2021-01-29 18:38:38 +0200

    Create Project Euler - 003.cpp

---
+++ Project Euler/Project Euler - 003.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        unsigned long long n;
+        cin >> n;
+        
+        unsigned long long l;
+        while(n%2==0){
+            n/=2;
+            l=2;
+        }
+            for(unsigned long long i=3; i*i<=n; i++){
+                while(n%i==0){
+                    n/=i;
+                }
+                l=i;
+            }
+            if(n!=1){
+                l=n;
+            }
+            std::cout<<l<<"\n";
+    }
+    return 0;
+}
